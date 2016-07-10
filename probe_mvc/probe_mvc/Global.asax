81a9e3a46436df993c2b9f416f9865f047291282
Anna-Lena <gertsij.elena@gmail.com>
2016-07-10 20:44:58 +0300

    probe_mvc

---
+++ probe_mvc/probe_mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="probe_mvc.MvcApplication" Language="C#" %>
---
+++ probe_mvc/probe_mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(probe_mvc.Startup))]
+namespace probe_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
